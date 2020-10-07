1b1e2f9746500579fa20c0b3a474d27339d3df42
snehal_iOS <svis.8994@gmail.com>
2020-10-07 09:59:55 +0400

    Initial Commit

---
+++ iOS_MVVM_demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_MVVM_demo
+//
+//  Created by Snehal on 10/7/20.
+//  Copyright © 2020 Snehal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
